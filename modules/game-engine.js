27650f6397cdea457ded74fa9c7f6a8e796d929c
yeromin <Dima.Yeromin@gmail.com>
2021-06-10 22:16:40 +0300

    task 5: finished

---
+++ modules/game-engine.js
@@ -0,0 +1,23 @@
+import readlineSync from 'readline-sync';
+import userName from '../src/cli.js';
+
+const ATTEMPT_COUNTER = 3;
+
+const gameEngine = (GAME_RULES_MESSAGE, generateGameData) => {
+  console.log(`${GAME_RULES_MESSAGE}`);
+
+  for (let i = 0; i < ATTEMPT_COUNTER; i += 1) {
+    const [curTask, curSolution] = generateGameData();
+    console.log(`Question: ${curTask}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+
+    if (userAnswer !== curSolution) {
+      console.log(`'${userAnswer}' is wrong answer ;( \nCorrect answer was '${curSolution}'. \nLet's try again, ${userName}!`);
+      return;
+    }
+    console.log('Correct!');
+  }
+  console.log(`Congratulations, ${userName}!`);
+};
+
+export default gameEngine;
--- src/cli.js
+++ src/cli.js
@@ -2,7 +2,6 @@ import readlineSync from 'readline-sync';
 
 console.log('Welcome to the Brain Games!');
 
-const greetingMessage = 'May I have your name?\n';
-const userName = readlineSync.question(greetingMessage);
+const userName = readlineSync.question('May I have your name? ');
 
 export default userName;
