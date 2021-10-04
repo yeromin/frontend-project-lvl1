9a7565b59ffe9e48998ec46a6a734dc11d3827a0
yeromin <Dima.Yeromin@gmail.com>
2021-10-04 20:56:52 +0300

    progression completed

---
+++ bin/brain-progression.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+import startProgressionGame from '../src/progression.js';
+
+startProgressionGame();
---
+++ src/progression.js
@@ -0,0 +1,27 @@
+import startGameEngine from '../modules/game-engine.js';
+import generateRandomNumber from '../modules/random-generator.js';
+
+const GAME_RULES_MESSAGE = 'What number is missing in the progression?';
+const LIMIT_RANDOM_NUMBER = 100;
+const MINIMAL_RANDOM_NUMBER = 0;
+const progrsMinLength = 5;
+const progrsMaxLength = 15;
+const minProgrsDifference = 1;
+const maxProgrsDifference = 20;
+
+function generateGameData() {
+  const firstRandomNumber = generateRandomNumber(MINIMAL_RANDOM_NUMBER, LIMIT_RANDOM_NUMBER);
+  const progrsDifference = generateRandomNumber(minProgrsDifference, maxProgrsDifference);
+  const progrsRandomLength = generateRandomNumber(progrsMinLength, progrsMaxLength);
+  const randomArrayElementIndex = generateRandomNumber(progrsMinLength, progrsRandomLength);
+  const generatedArr = [firstRandomNumber];
+  for (let i = 0; i < progrsRandomLength; i += 1) {
+    const nextNumber = (generatedArr[generatedArr.length - 1]) + progrsDifference;
+    generatedArr.push(nextNumber);
+  }
+  const correctSolution = generatedArr[randomArrayElementIndex];
+  generatedArr[randomArrayElementIndex] = '..';
+  return [generatedArr.join(' '), correctSolution.toString()];
+}
+
+export default () => startGameEngine(GAME_RULES_MESSAGE, generateGameData);
