5ca9e0afbcb628176aad190f3556f3addf13b5cc
yeromin <Dima.Yeromin@gmail.com>
2021-10-04 21:34:49 +0300

    fix generateRandomNumber

--- modules/random-generator.js
+++ modules/random-generator.js
@@ -1,3 +1,3 @@
-const generateRandomNumber = (number) => Math.floor(Math.random() * number);
+const generateRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
 export default generateRandomNumber;
