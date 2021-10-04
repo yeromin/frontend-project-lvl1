c0dbaf3c318f0430ecc5b045bf9f5ff3a7c12490
yeromin <Dima.Yeromin@gmail.com>
2021-10-04 20:46:35 +0300

    GCD task completed

---
+++ bin/brain-gcd.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+import startGCDGame from '../src/gcd.js';
+
+startGCDGame();
---
+++ src/gcd.js
@@ -0,0 +1,24 @@
+import startGameEngine from '../index.js';
+import generateRandomNumber from '../../modules/random-generator.js';
+
+const GAME_RULES_MESSAGE = 'Find the greatest common divisor of given numbers.';
+const LIMIT_RANDOM_NUMBER = 100;
+const MINIMAL_RANDOM_NUMBER = 0;
+
+const getSolution = (a, b) => {
+  if (b > 0) {
+    const x = a % b;
+    return getSolution(b, x);
+  }
+  return a;
+};
+
+function getRoundData() {
+  const curRandomFirst = generateRandomNumber(MINIMAL_RANDOM_NUMBER, LIMIT_RANDOM_NUMBER);
+  const curRandomSecond = generateRandomNumber(MINIMAL_RANDOM_NUMBER, LIMIT_RANDOM_NUMBER);
+  const curTask = `${curRandomFirst} ${curRandomSecond}`;
+  const curSolution = getSolution(curRandomFirst, curRandomSecond).toString();
+  return [curTask, curSolution];
+}
+
+export default () => startGameEngine(GAME_RULES_MESSAGE, getRoundData);
