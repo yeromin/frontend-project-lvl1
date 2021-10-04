11fcdbfa31d492d7099306c7ed2e354a5b955c51
yeromin <Dima.Yeromin@gmail.com>
2021-10-04 20:56:35 +0300

    prime completed

---
+++ src/prime.js
@@ -0,0 +1,26 @@
+import startGameEngine from '../index.js';
+import generateRandomNumber from '../../modules/random-generator.js';
+
+const GAME_RULES_MESSAGE = 'Answer \'yes\' if the number is prime, otherwise answer \'no\'.';
+const LIMIT_RANDOM_NUMBER = 100;
+const MINIMAL_RANDOM_NUMBER = 2;
+
+const isPrime = (number) => {
+  let flag = true;
+  for (let i = 2; i < number; i += 1) {
+    if (number % i === 0) {
+      flag = false;
+      break;
+    }
+  } return flag;
+};
+
+const convertAnswer = (number) => (isPrime(number) ? 'yes' : 'no');
+
+function generateGameData() {
+  const curTask = generateRandomNumber(MINIMAL_RANDOM_NUMBER, LIMIT_RANDOM_NUMBER);
+  const curSolution = convertAnswer(curTask);
+  return [curTask, curSolution];
+}
+
+export default () => startGameEngine(GAME_RULES_MESSAGE, generateGameData);
